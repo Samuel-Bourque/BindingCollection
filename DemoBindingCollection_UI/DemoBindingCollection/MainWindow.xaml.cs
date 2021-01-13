66cd37a7d9f938c0aeb2a557db20e7a5c41f5189
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 01:03:38 -0500

    Add files via upload

---
+++ DemoBindingCollection_UI/DemoBindingCollection/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoBindingCollection
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Prof> Profs { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Profs = new ObservableCollection<Prof>() {
+        new Prof("Luc Forcier"),
+        new Prof("Marco Guay"),
+        new Prof("Noémie Rochette"),
+        new Prof("Ridha Louati"),
+        new Prof("Gilles Portelance")
+    };
+            listeProfs.ItemsSource = Profs;
+        }
+
+        private void BtnAjouterNouveauProf_Click(object sender, RoutedEventArgs e)
+        {
+            Profs.Add(new Prof(tbNom.Text));
+        }
+
+        private void BtnMajProfSelectionne_Click(object sender, RoutedEventArgs e)
+        {
+            Prof unProf = listeProfs.SelectedItem as Prof;
+            if (unProf != null)
+                unProf.NomProf = tbNom.Text;
+        }
+
+        private void BtnSupprimerProf_Click(object sender, RoutedEventArgs e)
+        {
+            Prof unProf = listeProfs.SelectedItem as Prof;
+            Profs.Remove(unProf);
+
+        }
+    }
+}
---
+++ DemoBindingCollection_UI/DemoBindingCollection/Prof.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBindingCollection
+{
+    public class Prof : INotifyPropertyChanged
+    {
+        private string _nomProf;
+        public string NomProf
+        {
+            get => _nomProf;
+            set
+            {
+                _nomProf = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NomProf"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Prof(string nom) { NomProf = nom; }
+    }
+
+}
